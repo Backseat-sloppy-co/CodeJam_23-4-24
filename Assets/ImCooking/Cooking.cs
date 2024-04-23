0adac45bf88e1545a59b4e2426538f80e9eaaede
Akaliias <akaliasthegamer@gmail.com>
2024-04-23 14:58:42 +0200

    cooking

--- Assets/ImCooking/Cooking.cs
+++ Assets/ImCooking/Cooking.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Cooking : MonoBehaviour
 {
+
+    //Add a gameobject to the script of the steak
+    public GameObject steak;
+    public GameObject pan;
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(Cook());
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+
+    public IEnumerator Cook()
+    {
+        steak.GetComponent<Renderer>().material.color = Color.red;
+        yield return null;
+    }
+
 }
