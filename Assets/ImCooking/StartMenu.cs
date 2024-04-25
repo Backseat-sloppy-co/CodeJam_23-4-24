9e1b5f4db68e3ddcebe6b6956355f28b440af254
Victor Andersen <akaliasthegamer@gmail.com>
2024-04-25 14:43:48 +0200

    DESTROY :bomb:

--- Assets/ImCooking/StartMenu.cs
+++ Assets/ImCooking/StartMenu.cs
@@ -6,11 +6,14 @@ public class StartMenu : MonoBehaviour
 {
     public Camera mainCamera; // Reference to the main camera
     public Camera startMenuCamera; // Reference to the start menu camera
+    public GameObject startMenuGameObject;
 
     public void StartGame()
     {
         // Enable the main camera and disable the start menu camera
         mainCamera.depth = 1;
         startMenuCamera.depth = -1;
+        Destroy(startMenuCamera.gameObject);
+        Destroy(startMenuGameObject);
     }
 }
