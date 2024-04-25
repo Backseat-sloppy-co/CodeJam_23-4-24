04e62ce577d09a86a6fec5fbaea27d8d9862c869
Victor Andersen <akaliasthegamer@gmail.com>
2024-04-25 15:02:46 +0200

    cook

--- Assets/ImCooking/StartMenu.cs
+++ Assets/ImCooking/StartMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class StartMenu : MonoBehaviour
@@ -7,10 +8,12 @@ public class StartMenu : MonoBehaviour
     public Camera mainCamera; // Reference to the main camera
     public Camera startMenuCamera; // Reference to the start menu camera
     public GameObject startMenuGameObject;
+    public TMP_Text debugText;
 
     public void StartGame()
     {
         // Enable the main camera and destroy the start menu camera
+        debugText.text = "Starting game...";
         mainCamera.depth = 1;
         startMenuCamera.depth = -1;
         Destroy(startMenuCamera.gameObject);
