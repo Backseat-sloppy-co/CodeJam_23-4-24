db054b88038b13f1b5fdd1e41e4296452e9f3f75
Oskar B. Kaare <oskar.kaare@gmail.com>
2024-04-25 14:19:24 +0200

    d

--- Assets/Scripts/TargetShooter/TargetBehavior.cs
+++ Assets/Scripts/TargetShooter/TargetBehavior.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 using TMPro;
 
 class Target
+    // this class is used to store the target gameobject and a bool that is used to check if the target is the target that is currently set
 {
     public GameObject target;
     public bool isTarget;
+
+    // this constructor is used to set the target gameobject and set the isTarget bool to false
     public Target(GameObject target)
     {
         this.target = target;
@@ -31,7 +34,7 @@ public class TargetBehavior : MonoBehaviour
 
     private void Start()
     {
-        
+        // for as many as the count variable, instantiate a prefab at a location from the targetLocation list and add it to the targets list
         for (int i = 0; i < count; i++)
         {
             
@@ -40,7 +43,7 @@ public class TargetBehavior : MonoBehaviour
             target.transform.parent = transform;
             targets.Add(new Target(target));
         }
-
+        // set a random target from the targets list to be the target, and set isTarget to true and play the openUp animation and souunds
         var _target = targets[UnityEngine.Random.Range(0, targets.Count)];
         _target.isTarget = true;
         _target.target.GetComponent<Animator>().SetBool("openUp", true);
@@ -49,13 +52,15 @@ public class TargetBehavior : MonoBehaviour
     }
     private void Update()
     {
+        // if iswin is true, return out of update
         if (isWin)
         {
             return;
         }
-
+        // time is increased by the time since the last frame
         time += Time.deltaTime;
 
+        // on mouse click, start the shoot coroutine, destroy the "tryk" sprite, and check if the raycast hits an object tagged as "Enemy"
         if (Input.GetMouseButtonDown(0))
         {
             StartCoroutine(Shoot());
@@ -64,6 +69,7 @@ public class TargetBehavior : MonoBehaviour
             Debug.Log("Mouse is clicked");
             Destroy(tryk);
 
+            // if the raycast hits an object tagged as "Enemy", play the hitmarker sound, and check if it is the target that is currently set
             if (Physics.Raycast(ray, out hit))
             {
                 if(hit.collider.gameObject.CompareTag("Enemy"))
@@ -72,10 +78,12 @@ public class TargetBehavior : MonoBehaviour
                     Debug.Log("Target is clicked");
                     foreach (var target in targets)
                     {
+                        // 
                         if (target.target == hit.collider.gameObject)
                         {
                            Animator anim = target.target.GetComponent<Animator>();
                             Debug.Log("Target is found");
+                            // if the target is the target that is currently set, set isTarget to false, remove it from the targets list, play the hit animation, and set a new target
                             if(target.isTarget == true)
                             {
                                 target.isTarget = false;
@@ -94,6 +102,7 @@ public class TargetBehavior : MonoBehaviour
                 }
             }
         }
+        // this wincondition is called if all targets are hit
         if (IsAllTargetsHit())
         {
            AudioManager.instance.Play("Beep");
@@ -106,6 +115,7 @@ public class TargetBehavior : MonoBehaviour
     // this method checks if all targets are hit
     public bool IsAllTargetsHit()
     {
+        // this method checks if all bools in the targets list are false, as this means that all targets are hit
         foreach (var target in targets)
         {
             if (target.isTarget == true)
@@ -115,7 +125,7 @@ public class TargetBehavior : MonoBehaviour
         }
         return true;
     }
-    // enumerator courantine that waits for a few seconds
+    // this coroutine is called when the player shoots and plays the shooting animation and sound effect
     IEnumerator Shoot()
     {
         //play shooting animation
