6b6dbfdf9ba02608824cb34358e1837dc5b5f48d
Victor Andersen <akaliasthegamer@gmail.com>
2024-04-25 15:07:00 +0200

    text cone

--- Assets/ImCooking/StartMenu.cs
+++ Assets/ImCooking/StartMenu.cs
@@ -8,12 +8,11 @@ public class StartMenu : MonoBehaviour
     public Camera mainCamera; // Reference to the main camera
     public Camera startMenuCamera; // Reference to the start menu camera
     public GameObject startMenuGameObject;
-    public TMP_Text debugText;
+    
 
     public void StartGame()
     {
         // Enable the main camera and destroy the start menu camera
-        debugText.text = "Starting game...";
         mainCamera.depth = 1;
         startMenuCamera.depth = -1;
         Destroy(startMenuCamera.gameObject);
