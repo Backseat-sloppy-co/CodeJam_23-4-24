6f1284689f4b56e8009f5b33133e03f9b1cb0c49
Akaliias <akaliasthegamer@gmail.com>
2024-04-26 09:45:56 +0200

    Revert "Merge branch 'main' of https://github.com/Backseat-sloppy-co/CodeJam_23-4-24"
    This reverts commit de3637792963e674afc3aeafd522d089fe571f22, reversing
    changes made to 82d1bf34054d07d50940b9385eeba097e4a04efa.

--- Assets/Scripts/DinoJump/DeerBehavior.cs
+++ Assets/Scripts/DinoJump/DeerBehavior.cs
@@ -48,38 +48,19 @@ public class DeerBehavior : MonoBehaviour
         if (isGrounded)
         {
             if (Input.touchCount > 0)
-               StartCoroutine(jump());
+                jump();
 
             if (Input.GetMouseButtonDown(0))
-            StartCoroutine(jump());
+                jump();
         }
-        else
-        {
-            if (Input.touchCount > 0)
-                StartCoroutine(fall());
-
-            if (Input.GetMouseButtonDown(0))
-                StartCoroutine(fall());
-        }
-
-        IEnumerator jump()
+        void jump()
         {
             rb.velocity = Vector3.up * jumpForce;
-            AudioManager.instance.Play("Jump");
-            yield return new WaitForSeconds(0.5f); // wait for half a second
         }
 
-        IEnumerator fall()
-        {
-
-            rb.velocity = Vector3.down * jumpForce;
-            AudioManager.instance.Play("Down");
-            yield return new WaitForSeconds(0.5f); // wait for half a second
-        }
         // force the deer to be at the same z and x position
         transform.position = new Vector3(0, transform.position.y, 0);
         transform.rotation = rotation;
-
 
         if (lifeCounter == 0 && !isGameOverStarted)
         {
