4a960afb3a5d1aaa318eaafc8d126b4ab618d5de
backseat-sloppy <147695981+backseat-sloppy@users.noreply.github.com>
2024-05-24 10:06:50 +0200

    Update PenguinManager.cs

--- Assets/Scripts/DinoJump/PenguinManager.cs
+++ Assets/Scripts/DinoJump/PenguinManager.cs
@@ -2,21 +2,21 @@ using UnityEngine;
 
 public class PenguinManager : MonoBehaviour
 {
-    private int penguinSize;
+    private float penguinSize;
 
     [SerializeField] private GameObject penguinPrefab;
     private CapsuleCollider penguinCollider;
-    private int SpawnRate;
-    private Vector3 penguinSpawnPoint = new Vector3(12.7f, -1.42f, 0.28f);
+    private float SpawnRate;
+    private Vector3 penguinSpawnPofloat = new Vector3(12.7f, -1.42f, 0.28f);
 
-    private int spawnRMax = 6;
-    private int spawnRMin = 1;
-    private int rotationMax = 300;
-    private int rotationMin = 50;
-    private int speedMax = 3;
-    private int speedMin = 5;
-    private int sizeMax = 1;
-    private int sizeMin = 4;
+    private float spawnRMax = 6;
+    private float spawnRMin = 1;
+    private float rotationMax = 300;
+    private float rotationMin = 50;
+    private float speedMax = 3;
+    private float speedMin = 5;
+    private float sizeMax = 1;
+    private float sizeMin = 4;
 
 
 
@@ -33,7 +33,7 @@ public class PenguinManager : MonoBehaviour
             SpawnRate = spawnRMax;
         }
 
-        //spawn penguin at random interval between 1 and 6 seconds
+        //spawn penguin at random floaterval between 1 and 6 seconds
         if (Time.time > SpawnRate)
         {
             SpawnRate = Time.time + Random.Range(spawnRMin, spawnRMax);
@@ -43,17 +43,17 @@ public class PenguinManager : MonoBehaviour
 
     void SpawnPenguin()
     {
-        GameObject newPenguin = Instantiate(penguinPrefab, penguinSpawnPoint, Quaternion.Euler(0, 0, 0));
+        GameObject newPenguin = Instantiate(penguinPrefab, penguinSpawnPofloat, Quaternion.Euler(0, 0, 0));
         PenguinBehavior penguinBehavior = newPenguin.GetComponent<PenguinBehavior>();
 
         // Set the speed and size of the new penguin here
         penguinBehavior.penguinRotaionSpeed = Random.Range(rotationMin, rotationMax);
         penguinBehavior.penguinSpeed = Random.Range(speedMin, speedMax);
 
-        int size = Random.Range(speedMin, speedMax);
+        float size = Random.Range(speedMin, speedMax);
         newPenguin.transform.localScale = new Vector3(size, size, 1);
 
         Debug.Log("Penguin Found! Contact Authorities!!");
     }
 }
-// this code was written by me with the help of copilot.
+// this code was written with the help of copilot.
